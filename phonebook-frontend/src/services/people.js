c6bc24ea1279e6041738ed956a51caf88e020ae5
FranklyWeb11 <luke@franklyweb.nz>
2021-09-04 10:04:17 +1200

    part5 initial

---
+++ phonebook-frontend/src/services/people.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+const baseUrl = '/api/persons'
+
+
+const getPeople = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+const createPerson = (newPerson) => {
+    const request = axios.post(baseUrl, newPerson)
+    return request.then(response => response.data)
+}
+
+const deletePerson = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(request => request.data)
+}
+
+const updatePerson = (id, newPerson) => {
+    const request = axios.put(`${baseUrl}/${id}`, newPerson)
+    return request.then(request => request.data)
+}
+const peopleServices = {
+    createPerson, getPeople, deletePerson, updatePerson
+}
+
+export default peopleServices
